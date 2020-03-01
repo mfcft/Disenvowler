e74b90dab43f658a6f2a033bbdec8631b4789692
Marco <Marco@LAPTOP-T1KE82RE.lan>
2020-03-01 22:37:53 +0000

    Final fixing

--- disenvowler/src/disenvowler/Runner.java
+++ disenvowler/src/disenvowler/Runner.java
@@ -2,27 +2,31 @@ package disenvowler;
 
 public class Runner {
 	
+	
 	public void removevowels(String s) {
 		String vowels = s;
-		for(int i= 0; i<s.length(); i++) {
+		
+		for(int i= 0; i<s.length(); i++) { // iterates through the String characters
 			char c = s.charAt(i);
-			if(c=='a' || c=='e'||c=='i'||c=='o'||c=='u' || c ==' ') {
+			if(c=='a' || c=='e'||c=='i'||c=='o'||c=='u' || c ==' ') { // when a vowel or empty space is found it gets removed
 				String a ="" + c;
-				s = s.replace(a, "");
+				s = s.replace(a, ""); 
 				
 			}
 		}
 		System.out.println("Consonantes: " + s);
 		System.out.println("Vogais: " + getvowels(vowels));
 	}
-	public String getvowels(String s) {
+	
+	
+	public String getvowels(String s) { // method that returns a string with only the vowels of another given string
 		String vowels = new String();
-		for(int i= 0; i<s.length(); i++) {
+		for(int i= 0; i<s.length(); i++) { // iterate the string
 			char c = s.charAt(i);
-			if(c=='a' || c=='e'||c=='i'||c=='o'||c=='u' || c ==' ') {
-				String a ="" + c;
+			if(c=='a' || c=='e'||c=='i'||c=='o'||c=='u' || c ==' ') { // finds vowels or spaces
+				String a = "" + c; //convert char to string
 					if(!a.equals(" ")) {
-					vowels = vowels.concat(a);
+					vowels = vowels.concat(a); // add char each time a vowel is found
 					}	
 			}
 		}
